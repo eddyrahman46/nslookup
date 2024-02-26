c03ab0ac1767d020c608d40a4a893075751758bd
noc iplus <noc@iplus.net.id>
2024-02-27 05:06:30 +0700

    first commit

---
+++ LookingGlass/Config.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * LookingGlass - User friendly PHP Looking Glass
+ *
+ * @package     LookingGlass
+ * @author      Nick Adams <nick@iamtelephone.com>
+ * @copyright   2015 Nick Adams.
+ * @link        http://iamtelephone.com
+ * @license     http://opensource.org/licenses/MIT MIT License
+ * @version     1.3.0
+ */
+
+// IPv4 address
+$ipv4 = '103.214.100.2';
+// IPv6 address (can be blank)
+$ipv6 = '';
+// Rate limit
+$rateLimit = (int) '0';
+// Site name (header)
+$siteName = 'IPLUS';
+// Site URL
+$siteUrl = 'http://lg.iplus.net.id';
+// Server location
+$serverLocation = 'Batam';
+// Test files
+$testFiles = array();
+$testFiles[] = '25MB';
+$testFiles[] = '50MB';
+$testFiles[] = '100MB';
+$testFiles[] = '1000MB';
+// Theme
+$theme = 'spacelab';
